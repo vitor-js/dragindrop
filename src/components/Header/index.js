cdc35c8bb9c17d9f247b8e25359d402901481b54
vitorgabriel9812@outlook.com <vitor.melo@onsurance.me>
2021-01-21 03:23:44 -0300

    dragindrop

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <h1>Estudos de Drag´n Drop</h1>
+    </Container>
+  );
+}
